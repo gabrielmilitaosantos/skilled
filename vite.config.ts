9a7248d989818d1ff6324c4bc5f88bd5808ab6e7
Gabriel Militão <gabrielmilitaosantos@gmail.com>
2026-06-26 11:01:18 -0300

    Fix some mismatch errors from React Compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
-import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import viteReact, { reactCompilerPreset } from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 const config = defineConfig({
 	resolve: { tsconfigPaths: true },
-	plugins: [
-		tailwindcss(),
-		tanstackStart(),
-		nitro(),
-		viteReact(),
-		babel({ presets: [reactCompilerPreset()] }),
-	],
+	plugins: [tailwindcss(), tanstackStart(), nitro(), viteReact()],
 	server: {
 		allowedHosts: true,
 	},
